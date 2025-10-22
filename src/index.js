ba835d36c1b3b7e08c0c533d9326c4cc4472a3d4
e1ectron1k <tron1k@yandex.ru>
2025-10-22 21:15:58 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
-const ajs = require('@e1ectron1k/ajs-git'); console.log('Full object:', ajs); console.log('Type:', typeof ajs);
+const ajs = require('@e1ectron1k/ajs-git');
+console.log(ajs.info());
